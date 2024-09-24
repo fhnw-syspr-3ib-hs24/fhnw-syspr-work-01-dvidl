4fbf9b3abbf8c3ec8f8d0bbd34c425fe45918438
David Locher <david.locher@students.fhnw.ch>
2024-09-24 17:05:11 +0200

    add exercise args.c

---
+++ args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+	for(int i = 1; i < argc; i++) {
+		int valid = 1;
+		for(int j = 0; argv[i][j] != 0; j++) {
+			if (argv[i][j] < 'a' | argv[i][j] > 'z') {
+				valid = 0;
+			}
+		}
+		if (valid) {
+		printf("Argument %d: %s\n", i, argv[i]);
+		} else {
+			printf("Argument %d: %s is invalid\n", i, argv[i]);
+		}
+	}
+	return 0;
+}
